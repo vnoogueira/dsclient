e2641fb10e81dff62303fd9ad94ae0ba27d9c875
Vitor Nogueira <vitor.nogueira94@gmail.com>
2022-08-09 22:12:46 -0300

    Created method findAll(Pageable), insertClient and updateClient

--- backend/src/main/java/com/vitorferreira/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/vitorferreira/dsclient/resources/ClientResource.java
@@ -1,11 +1,21 @@
 package com.vitorferreira.dsclient.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vitorferreira.dsclient.dto.ClientDTO;
 import com.vitorferreira.dsclient.services.ClientService;
@@ -22,5 +32,33 @@ public class ClientResource {
 		ClientDTO dtoId = service.findById(id);
 		return ResponseEntity.ok().body(dtoId);
 	}
+
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = service.findAll(pageRequest);		
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insertClient(@RequestBody ClientDTO obj){
+		ClientDTO clients = service.insertClient(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clients.getId()).toUri();
+		return ResponseEntity.created(uri).body(clients);
+		
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<ClientDTO> updateClient(@PathVariable Long id, @RequestBody ClientDTO obj){
+		ClientDTO entity = service.updateClient(id, obj);
+		return ResponseEntity.ok().body(entity);
+		
+	}
+	
+	
 
 }
--- backend/src/main/java/com/vitorferreira/dsclient/services/ClientService.java
+++ backend/src/main/java/com/vitorferreira/dsclient/services/ClientService.java
@@ -2,7 +2,11 @@ package com.vitorferreira.dsclient.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +26,41 @@ public class ClientService {
 		Optional<Client> client = repository.findById(id);
 		Client entity = client.orElseThrow(() -> new ResourceNotFoundException("Client Not Found"));
 		return new ClientDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(obj -> new ClientDTO(obj));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO insertClient(ClientDTO obj) {
+		Client entity = new Client();
+		copyEntityToDto(obj, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+
+	}
+
+	@Transactional
+	public ClientDTO updateClient(Long id, ClientDTO obj) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			copyEntityToDto(obj, entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+
+	}
+
+	private void copyEntityToDto(ClientDTO obj, Client entity) {
+		entity.setName(obj.getName());
+		entity.setCpf(obj.getCpf());
+		entity.setIncome(obj.getIncome());
+		entity.setBirthDate(obj.getBirthDate());
+		entity.setChildren(obj.getChildren());
 	}
 
 }
